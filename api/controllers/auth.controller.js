70b40b27142b5cea34a9c789504fed43cc32ad6b
LingarajMantha <lingarajmantha10@gmail.com>
2024-03-23 21:32:33 +0530

    creatung a middleware and a function to hab=ndle a possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
 
-export const signup =  async (req, res)=>{
+export const signup =  async (req, res, next)=>{
 
     const{username, email, password} = req.body;
 
@@ -12,7 +13,8 @@ export const signup =  async (req, res)=>{
         await newUser.save()
         res.status(201).json("user created sucessfully");
     }catch(error){
-        res.status(500).json(error.message);
+        next(errorHandler(550, 'error from the function'));  //this error message  is showed when you send user name or email dupication while entering the user 
+        //we are using the error handling form the middleware ,, when we use next it prints the error that is picing form the index.js
     }
 }
 
